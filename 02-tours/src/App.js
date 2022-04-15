454209f20590b8b027c98bc038774acf8e655b24
VittorGuih <vittytuuguilherme@gmail.com>
2022-04-15 19:32:00 -0300

    feat(tours): add tour project]

---
+++ 02-tours/src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+  if (tours.length === 0 && !loading) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>No tours left</h2>
+          <button className='btn' onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      {loading ? <Loading /> : <Tours tours={tours} removeTour={removeTour} />}
+    </main>
+  );
+}
+
+export default App;
